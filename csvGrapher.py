bb11a10a6c56691bf4e3922ed696293dd73bc583
Abhay Cashikar <abhaycashikar@gmail.com>
2019-04-06 11:53:02 -0400

    Removed API key from code, added comments

--- csvGrapher.py
+++ csvGrapher.py
@@ -3,25 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate
 
+filename = 'darkskytest'
+
+# Turn on interactive mode, required to display on Mac
 plt.ion()
 
-with open('darkskytest.csv', 'r') as f:
+# Read CSV into two-dimensional list
+with open(filename + '.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
 
+# Extract cloud cover data and convert to floats
 strs = [i[2:] for i in data[1:]]
 cloudCover = [list(map(float, i)) for i in strs]
+# Create meshgrid of time and distance
 lx, lt = len(cloudCover), len(cloudCover[0])
 x, t = range(0, lx), range(0, lt)
 xi, ti = np.meshgrid(x, t)
-grad = np.gradient(cloudCover)
-mag = np.sqrt(grad[0]**2 + grad[1]**2)
 
+# Initialize and display figure
 fig = plt.figure()
-rbf = scipy.interpolate.Rbf(xi, ti, cloudCover)
 plt.imshow(cloudCover, vmin = np.amin(cloudCover), vmax=np.amax(cloudCover))
 plt.xlabel('Time (hours)')
 plt.ylabel('Distance')
 plt.yticks([0,lx-1], ['Start','End'])
 plt.colorbar()
+plt.set_cmap('jet')
+# Make 0 on y-axis meet 0 on x-axis
 plt.gca().invert_yaxis()
--- csvMaker.py
+++ csvMaker.py
@@ -2,29 +2,42 @@ import csv
 from datetime import datetime
 import requests
 
-with open('ASC 2018 Segment 1_truncated.txt', 'r') as f:
-  reader = csv.reader(f)
-  segment = list(reader)
+# File to extract coordinates from
+filename = 'ASC 2018 Segment 1_truncated'
 
+# API key stored locally in text file
+with open('APIKey.txt','r') as f:
+    key = f.readline().rsplit()[0]
+
+# Get a two-dimensional list from race data
+with open(filename + '.txt', 'r') as f:
+    reader = csv.reader(f)
+    segment = list(reader)
+
+# Make a request to extract time data
 coordinates = [i[2:4] for i in segment[1::2]]
-URL = 'https://api.darksky.net/forecast/9ee4a75602f09db95f5b1eeee3fa2910/' + coordinates[0][0] + ',' + coordinates[0][1]
+URL = 'https://api.darksky.net/forecast/' + key + '/' + coordinates[0][0] + ',' + coordinates[0][1]
 exclude = 'currently,minutely,daily,alerts,flags'
 PARAMS = {'exclude':exclude}
 r = requests.get(url = URL, params = PARAMS).json()
+print('Connection made! Extracting data...')
+# Initialize lists for cloud cover and time data
 cloudCover = [[None]*(len(r['hourly']['data'])+2) for i in range(len(coordinates))]
 time = [None]*(len(r['hourly']['data'])+2)
 time[0:2] = ['Latitude','Longitude']
+# Write sampled times to the time list
 for i, data in enumerate(r['hourly']['data']):
     time[i+2] = datetime.utcfromtimestamp(data['time']).strftime("%m/%d/%Y, %H:%M:%S")
-
+# Make a request for every lat and lon in coordinates, then write to cloudCover
 for i, [lat,lon] in enumerate(coordinates):
     cloudCover[i][0:2] = [lat,lon]
     URL = 'https://api.darksky.net/forecast/9ee4a75602f09db95f5b1eeee3fa2910/' + lat + ',' + lon
     r = requests.get(url = URL, params = PARAMS).json()
     for j, data in enumerate(r['hourly']['data']):
         cloudCover[i][j+2] = data['cloudCover']
-        
-with open('darkskytest.csv', 'w') as csv_file:
+
+# Write time and cloud cover data to CSV file
+with open(filename + '.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(time)
     writer.writerows(cloudCover)
